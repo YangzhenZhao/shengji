09d99432ce622e8ed319fe0ee04a3e949c162796
zhaoyz <954241552@qq.com>
2022-04-05 19:04:38 +0800

    Install socket.io

--- front_end/src/components/login.tsx
+++ front_end/src/components/login.tsx
@@ -4,18 +4,25 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { red, grey } from '@mui/material/colors';
+import Button from '@mui/material/Button';
 
 export const LoginScreen = () => {
     return (
-        <Card sx={{ display: 'flex', height: '100vh' }}>
-            <CardContent sx={{bgcolor: red[500]}}>
+        <Card sx={{ height: '100vh' }}>
+            <CardContent sx={{bgcolor: red[500], height: '5vh'}}>
                 <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
                     欢迎来到欢乐升级!
                 </Typography>
             </CardContent>
-            <CardContent sx={{ flex: '1 0 auto', bgcolor: grey[500] }}>
+            <CardContent 
+                sx={{ 
+                    height: '95vh',
+                    bgcolor: grey[500], 
+                    textAlign: "center",
+                }}
+            >
                 <TextField 
-                    label="请输入一个昵称开始游戏" 
+                    label="请输入一个昵称" 
                     color="error" 
                     focused 
                     margin='normal'
@@ -23,6 +30,14 @@ export const LoginScreen = () => {
                         fontSize: "40"
                     }}
                 />
+                <CardContent
+                    sx={{ 
+                        bgcolor: grey[500], 
+                        textAlign: "center",
+                    }}
+                >
+                    <Button variant="contained" color="success" size="large">开始游戏</Button>
+                </CardContent>
             </CardContent>
         </Card>
     )

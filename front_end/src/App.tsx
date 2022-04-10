da46714c88bdac71576831d7ce0939f5700cec51
zhaoyz <954241552@qq.com>
2022-04-10 15:20:18 +0800

    Use webscoket instead of socket.io

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { LoginScreen } from './components/login'
+import { GameScreen } from './components/game'
 
 function App() {
   return (
-    <LoginScreen></LoginScreen>
+    <GameScreen></GameScreen>
+    // <LoginScreen></LoginScreen>
   );
 }
 
---
+++ front_end/src/components/game.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const GameScreen = () => {
+    return (
+        <div>
+            hhh
+        </div>
+    )
+}
